4a8c338cc0bd6159932714dbaed054a74f5c8928
Friedrich von Never <friedrich@fornever.me>
2015-12-02 21:24:33 +0600

    Add a couple of metal objects.

--- Tools/Generate-CSX.m
+++ Tools/Generate-CSX.m
@@ -11,4 +11,11 @@ CSX = AddBox(CSX, 'excitation', 0, [-10 -10 0], [10 10 0]);
 CSX = AddDump(CSX, 'Et');
 CSX = AddBox(CSX, 'Et', 0, [-10 0 -10], [10 0 30]);
 
+CSX = AddMetal(CSX, 'Object 1');
+CSX = AddMetal(CSX, 'Object 2');
+
+CSX = AddBox(CSX, 'Object 1', 0, [0 0 0], [1 1 1]);
+CSX = AddBox(CSX, 'Object 1', 0, [7 7 7], [8 8 8]);
+CSX = AddBox(CSX, 'Object 2', 0, [3 3 3], [5 5 5]);
+
 struct_2_xml('./CSX.xml', CSX, 'ContinuousStructure');
