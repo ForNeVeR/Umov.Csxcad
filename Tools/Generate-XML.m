89096887b8c19f3c145382efbbcf76ad62a850a7
Friedrich von Never <friedrich@fornever.me>
2016-01-07 19:58:28 +0600

    Tools: add a script to generate openEMS XML

--- Tools/Generate-XML.m
+++ Tools/Generate-XML.m
@@ -19,3 +19,9 @@ CSX = AddBox(CSX, 'Object 1', 0, [7 7 7], [8 8 8]);
 CSX = AddBox(CSX, 'Object 2', 0, [3 3 3], [5 5 5]);
 
 struct_2_xml('./CSX.xml', CSX, 'ContinuousStructure');
+
+FDTD = InitFDTD();
+FDTD = SetSinusExcite(FDTD, 10e6);
+FDTD = SetBoundaryCond(FDTD, {'PMC' 'PMC' 'PEC' 'PEC' 'MUR' 'MUR'});
+
+WriteOpenEMS('./openEMS.xml', FDTD, CSX);
