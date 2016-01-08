631d759d2fc20f524f006d29ba2a87da5e5f8835
Friedrich von Never <friedrich@fornever.me>
2016-01-08 12:22:19 +0600

    Tests: add tests for CsxReader

--- Tesla.Csxcad.Tests/UtilsTests.fs
+++ Tesla.Csxcad.Tests/UtilsTests.fs
@@ -8,13 +8,13 @@ open Tesla.Csxcad.Properties
 open Tesla.Csxcad
 
 [<Fact>]
-let ``Utils.parseEnum should parse enums properly``() =
+let ``Utils.parseEnum should parse enums properly`` () =
     let enumMember = DumpType.EFrequencyTimeDomain
     let value = string <| int enumMember
     Assert.Equal (enumMember, Utils.parseEnum value)
 
 [<Fact>]
-let ``Utils.parseEnum should throw exception for unknown enum member``() =
+let ``Utils.parseEnum should throw exception for unknown enum member`` () =
     let value = "777"
     let action = Action (fun () ->
                              let value : DumpType = Utils.parseEnum value
