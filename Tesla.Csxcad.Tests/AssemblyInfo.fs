314f3ca91f858d89d6d73ebb9f591aac4d20227d
Friedrich von Never <friedrich@fornever.me>
2016-01-09 19:04:32 +0600

    Project: proper dashes in copyrights

--- Tesla.Csxcad.Tests/AssemblyInfo.fs
+++ Tesla.Csxcad.Tests/AssemblyInfo.fs
@@ -5,7 +5,7 @@ open System.Runtime.InteropServices
 
 [<assembly: AssemblyTitle("Tesla.Csxcad")>]
 [<assembly: AssemblyProduct("Tesla")>]
-[<assembly: AssemblyCopyright("Copyright © F. von Never 2015-2016")>]
+[<assembly: AssemblyCopyright("Copyright © F. von Never 2015–2016")>]
 [<assembly: ComVisible(false)>]
 [<assembly: Guid("2272a8fe-8d64-4e14-8b93-2a7c9ab309cf")>]
 [<assembly: AssemblyInformationalVersion("0.0.2.0")>]
--- Tesla.Csxcad/AssemblyInfo.fs
+++ Tesla.Csxcad/AssemblyInfo.fs
@@ -5,7 +5,7 @@ open System.Runtime.InteropServices
 
 [<assembly: AssemblyTitle("Tesla.Csxcad")>]
 [<assembly: AssemblyProduct("Tesla")>]
-[<assembly: AssemblyCopyright("Copyright © F. von Never 2015-2016")>]
+[<assembly: AssemblyCopyright("Copyright © F. von Never 2015–2016")>]
 [<assembly: ComVisible(false)>]
 [<assembly: Guid("7dec457d-1fc6-475b-adc0-6382e8558306")>]
 [<assembly: AssemblyInformationalVersion("0.0.2.0")>]
