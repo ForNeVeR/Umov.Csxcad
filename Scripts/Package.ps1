44879c0e92862f9e883b931e8f264311bc50b35d
Friedrich von Never <friedrich@fornever.me>
2016-02-06 16:51:12 +0600

    #15: add GitLink support
    Close #15.

--- Scripts/Package.ps1
+++ Scripts/Package.ps1
@@ -5,24 +5,35 @@
     Path to the MSBuild executable.
 .PARAMETER nuget
     Path to the Nuget executable.
+.PARAMETER GitLink
+    Path to the GitLink executable.
 .PARAMETER Solution
     Path to the solution that will be built.
 .PARAMETER Project
     Path to the main project that will be packed.
+.PARAMETER ProjectUrl
+    GitHub URL for GitLink.
 .PARAMETER Configuration
     Solutuion configuration that will be built and packed.
 #>
 param (
     $msbuild = 'msbuild',
     $nuget = 'nuget',
+    $GitLink = "$PSScriptRoot/../packages/gitlink/lib/net45/GitLink.exe",
     $Solution = "$PSScriptRoot/../Tesla.Csxcad.sln",
     $Project = "$PSScriptRoot/../Tesla.Csxcad/Tesla.Csxcad.fsproj",
-    $Configuration = "Release"
+    $ProjectUrl = 'https://github.com/ForNeVeR/Tesla.Csxcad',
+    $Configuration = 'Release'
 )
 
 $ErrorActionPreference = 'Stop'
 
 & $msbuild $Solution /m "/p:Configuration=$Configuration" /p:Platform="Any CPU"
+if (-not $?) {
+    exit $LASTEXITCODE
+}
+
+& $GitLink . -u $ProjectUrl
 if (-not $?) {
     exit $LASTEXITCODE
 }
