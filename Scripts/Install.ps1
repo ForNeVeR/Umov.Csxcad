2050f567ffa73a1a28468205e3f845ffee109f19
Friedrich von Never <friedrich@fornever.me>
2016-06-24 21:50:22 +0700

    Install script: add hash checking

--- Scripts/Install.ps1
+++ Scripts/Install.ps1
@@ -8,7 +8,8 @@
 #>
 param (
     [string] $Directory = "$PSScriptRoot/../.paket",
-    [string] $PaketVersion = "2.40.11"
+    [string] $PaketVersion = "2.40.11",
+    [string] $PaketBootstrapperSha256Hash = '6B770E158CB196B032F39CC1788BB2A29904C5726F5E9B297EEF9709ED088E71'
 )
 
 $ErrorActionPreference = 'Stop'
@@ -24,6 +25,11 @@ $bootstrapper = "$Directory/paket.bootstrapper.exe"
 if (-not (Test-Path $bootstrapper)) {
     Write-Output "Downloading paket bootstrapper"
     Invoke-WebRequest $url -OutFile $bootstrapper
+    $hash = (Get-FileHash $bootstrapper -Algorithm SHA256).Hash
+    if ($hash -ne $PaketBootstrapperSha256Hash) {
+        throw "Invalid Paket bootstrapper hash. Expected $PaketBootstrapperSha256Hash, got $hash"
+        Remove-Item $bootstrapper
+    }
 }
 
 if (-not $?) {
